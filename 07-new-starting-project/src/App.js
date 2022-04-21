eeb9ee4fe2a8b561631f413174b1ecb5ac54bb03
Aleksandra Petryka <aleksandra.petryka@gmail.com>
2022-04-21 09:52:18 +0200

    add secja 29 up to 476

--- 07-new-starting-project/src/App.js
+++ 07-new-starting-project/src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetups from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
+import Layout from './components/layout/Layout';
+
 function App() {
-  return <div></div>;
+  return <Layout>
+    <Switch>
+      <Route path="/" exact>
+        <AllMeetups/>
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage/>
+      </Route>
+      <Route path="/favorites">
+        <Favorites/>
+      </Route>
+    </Switch>
+  </Layout>;
 }
 
 export default App;
